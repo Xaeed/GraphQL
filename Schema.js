ff1037e5f8f1242db842e279ba4da409b4ba101c
xaeed <saeedbutt320@gmail.com>
2018-09-02 03:44:36 +0200

    edit customer

--- Schema.js
+++ Schema.js
@@ -88,7 +88,24 @@ fields:{
         }
 
 
-    }
+    },
+    editCustomer:
+    {
+        type:CustomerType,
+        args:
+        {
+           id:{ type: new GraphQLNonNull(GraphQLString)},
+           name:{ type: GraphQLString},
+           email:{ type: GraphQLString},
+           age:{ type: GraphQLInt}
+        },
+        resolve(parentValue,args)
+        {
+            return axios.patch('http://localhost:3000/customers/'+args.id,args)
+             .then(res => res.data);
+        }
+
+    },
 
 }});
 
