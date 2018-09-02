5338580ac65cbf2427b8bbb1b0009f10bf279395
xaeed <saeedbutt320@gmail.com>
2018-09-02 03:34:02 +0200

    add and delete data

--- Schema.js
+++ Schema.js
@@ -47,12 +47,53 @@ const RootQuery = new GraphQLObjectType({
                     .then(res => res.data);
             }
         }
-    }
-}
+    }});
 
-);
+
+const mutation = new GraphQLObjectType({
+name:'Mutations',
+fields:{
+    addCustomer:
+    {
+        type:CustomerType,
+        args:
+        {
+          
+           name:{ type: new GraphQLNonNull(GraphQLString)},
+           email:{ type: new GraphQLNonNull(GraphQLString)},
+           age:{ type: new GraphQLNonNull(GraphQLInt)}
+        },
+        resolve(parentValue,args)
+        {
+            return axios.post('http://localhost:3000/customers/',{
+                name:args.name,
+                email:args.email,
+                age:args.age
+             })
+             .then(res => res.data);
+        }
+
+    },
+    deleteCustomer:
+    {
+        type:CustomerType,
+        args:
+        {
+            id:{type: new GraphQLNonNull(GraphQLInt)}
+        },
+        resolve(parentValue,args)
+        {
+            return axios.delete('http://localhost:3000/customers/'+args.id)
+            .then( res => res.data);
+        }
+
+
+    }
+
+}});
 
 module.exports = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
+    mutation
 });
 
